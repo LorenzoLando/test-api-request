155d6957a43b633c9c4bbde11822496a132128d4
Lorenzo Lando <llando@google.com>
2019-03-07 19:04:40 +0000

    iniziata richiesta ajax

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,32 @@
-$.ajax({
-    url: 'https://randomuser.me/api/?format=xml',
-    dataType: 'xml',
-    success: function(data) {
-      console.log(data);
+// api request example
+// $.ajax({
+//     url: 'https://randomuser.me/api/?format=xml',
+//     dataType: 'xml',
+//     success: function(data) {
+//       console.log(data);
+//     }
+//   });
+
+
+// esempio richiesta ajax con javascript 
+//creo l`oggetto
+let xhr = new XMLHttpRequest();
+//creo la callback 
+xhr.onreadystatechange = function () {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        console.log(data);
+        let dataRaffinati = data.results[0];
+        console.log(dataRaffinati);
+       let generatedHTML = `
+       <ul>
+            <li>First Name: ${}
+       
+       `
     }
-  });
-        
+};
+//apro la richiesta 
+xhr.open('GET', 'https://randomuser.me/api/?format=JSON');
+//mando la richiesta
+xhr.send();
+ 
