81ca4bfefe7954260181d67a22d776deb809e3d1
LorenzoLando <lorenzo.lando85@gmail.com>
2019-03-08 22:32:21 +0000

    impaginazione

--- js/scripts.js
+++ js/scripts.js
@@ -8,33 +8,60 @@
 //   });
 // esempio richiesta ajax con javascript 
 //creo l`oggetto
-let xhr = new XMLHttpRequest();
-//creo la callback 
-xhr.onreadystatechange = function () {
-    if(xhr.readyState === 4 && xhr.status === 200) {
-        let data = JSON.parse(xhr.responseText);
-        let refinedData = data.results[0];
-        console.log(refinedData);
-        let generatedHTML = `
-            <div class="card">
-                <div class="card-img-container">
-                    <img class="card-img" src="${refinedData.picture.medium}" alt="profile picture">
+let galleryContainer = document.querySelector('#gallery');
+let apiRequest  = () => {
+ let xhr = new XMLHttpRequest();
+    //creo la callback 
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            let data = JSON.parse(xhr.responseText);
+            let refinedData = data.results[0];
+            console.log(refinedData);
+            let generatedHTML = `
+                <div class="card">
+                    <div class="card-img-container">
+                        <img class="card-img" src="${refinedData.picture.medium}" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${refinedData.name.first} ${refinedData.name.last}</h3>
+                        <p class="card-text">${refinedData.email}</p>
+                        <p class="card-text cap">${refinedData.location.city}, ${refinedData.location.state}</p>
+                    </div>
                 </div>
-                <div class="card-info-container">
-                    <h3 id="name" class="card-name cap">${refinedData.name.first} ${refinedData.name.last}</h3>
-                    <p class="card-text">${refinedData.email}</p>
-                    <p class="card-text cap">${refinedData.location.city}, ${refinedData.location.state}</p>
-                </div>
-            </div>
-        `;
-        console.log(generatedHTML);
-        
-    }
-};
-//apro la richiesta 
-xhr.open('GET', 'https://randomuser.me/api/?format=JSON');
-//mando la richiesta
-xhr.send();
- 
+            `;
+            galleryContainer.innerHTML += generatedHTML;
+            
+            
+        }
+    };
+    //apro la richiesta 
+    xhr.open('GET', 'https://randomuser.me/api/?format=JSON');
+    //mando la richiesta
+    xhr.send();
+}
+
+for() {
+
+}
+
+apiRequest();
+apiRequest();
+apiRequest();
+apiRequest();
+apiRequest();
+apiRequest();
+apiRequest();
+apiRequest();
+apiRequest();
+apiRequest();
+
+//devi controllare che la stessa card non venga ripetuta piu volte
+//se clikki su una immagine si deve aprire un modale
+
+
+
+
+
+
 
 
