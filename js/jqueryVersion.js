b5b3b8f499c61f71b5a87165c56f66e43198216c
LorenzoLando <lorenzo.lando85@gmail.com>
2019-03-13 20:00:13 +0000

    Prima versione che funziona

--- js/jqueryVersion.js
+++ js/jqueryVersion.js
@@ -83,8 +83,7 @@ $.getJSON('https://randomuser.me/api/?results=12', function(data) {
 
 
     let refinedData = data.results;
-    console.log(refinedData);
-    refinedData.forEach(item => {
+    refinedData.forEach((item, index) => {
     let generatedHTML = `
             <div class="card">
                 <div class="card-img-container">
@@ -97,21 +96,38 @@ $.getJSON('https://randomuser.me/api/?results=12', function(data) {
                 </div>
             </div>
             `;
-        $('#gallery').append(generatedHTML);    
-    
-    
-    
+
+            let generatedModal = `
+                <div class="modal-container">
+                    <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                    <img class="modal-img" src="${item.picture.thumbnail}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${item.name.first} ${item.name.last}</h3>
+                    <p class="modal-text">${item.email}</p>
+                    <p class="modal-text cap">${item.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${item.phone}</p>
+                    <p class="modal-text">${item.location.street}</p>
+                    <p class="modal-text">Birthday: ${item.dob.date}</p>
+                    </div>
+                </div>
+            `;
+        $('#gallery').append(generatedHTML); 
+        document.querySelectorAll('.card')[index].addEventListener('click', function() {
+            $('#gallery').append(generatedModal);
+            $('.modal-close-btn').on('click', function() {
+                  $('.modal-container').fadeOut();  
+            });
+        });   
     });
-        
-
-
-    
 });
 
-
-
-
-
+document.querySelectorAll('.modal-close-btn').forEach(function(item){
+    item.addEventListener('click', function() {
+        
+    });
+});
 
 
 
