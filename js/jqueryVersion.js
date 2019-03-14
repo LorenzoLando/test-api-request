f4efac8ff2d61fccee40bf69bbbc9d48e7a1c926
Lorenzo Lando <llando@google.com>
2019-03-14 18:58:06 +0000

    ugly working version

--- js/jqueryVersion.js
+++ js/jqueryVersion.js
@@ -1,21 +1,4 @@
-// class Card {
-//     constructor(numberUsers) {
-//         this.url = `https://randomuser.me/api/?results=${numberUsers}`,
-        
-//     }
-
-
-   
-    
-     
-// });
-
-
-
-
-
-const callBack = (data) => {
-
+$.getJSON('https://randomuser.me/api/?results=12', function (data) {
     let refinedData = data.results;
     refinedData.forEach((item, index) => {
     let generatedHTML = `
@@ -29,43 +12,91 @@ const callBack = (data) => {
                 <p class="card-text cap">${item.location.city}, ${item.location.state}</p>
                 </div>
             </div>
-            `;
+        `;
 
-            let generatedModal = `
-                <div class="modal-container">
-                    <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                    <img class="modal-img" src="${item.picture.thumbnail}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${item.name.first} ${item.name.last}</h3>
-                    <p class="modal-text">${item.email}</p>
-                    <p class="modal-text cap">${item.location.city}</p>
-                    <hr>
-                    <p class="modal-text">${item.phone}</p>
-                    <p class="modal-text">${item.location.street}</p>
-                    <p class="modal-text">Birthday: ${item.dob.date}</p>
-                    </div>
-                </div>
-            `;
+        let generatedModal = `
+        <div class="modal-container">
+            <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+            <img class="modal-img" src="${item.picture.thumbnail}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${item.name.first} ${item.name.last}</h3>
+            <p class="modal-text">${item.email}</p>
+            <p class="modal-text cap">${item.location.city}</p>
+            <hr>
+            <p class="modal-text">${item.phone}</p>
+            <p class="modal-text">${item.location.street}</p>
+            <p class="modal-text">Birthday: ${item.dob.date}</p>
+            </div>
+        </div>
+        `;
+
         $('#gallery').append(generatedHTML); 
         document.querySelectorAll('.card')[index].addEventListener('click', function() {
             $('#gallery').append(generatedModal);
             $('.modal-close-btn').on('click', function() {
                   $('.modal-container').fadeOut();  
             });
-        });   
-    });
-};
+        });
+    }); //end for each
+    console.log(data);
+} //end call back
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+         
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.querySelectorAll('.modal-close-btn').forEach(function(item){
     item.addEventListener('click', function() {
         
     });
 
-});
-
-$.getJSON('https://randomuser.me/api/?results=12',  callBack(data));
-
+}); 
 
 
 
